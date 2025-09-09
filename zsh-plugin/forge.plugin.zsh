303d4504a43cbdec6c30915b98ec9513d0fe11c1
Tushar Mathur <tusharmath@gmail.com>
2025-09-09 10:36:18 +0530

    chore: update plugin cmd

--- zsh-plugin/forge.plugin.zsh
+++ zsh-plugin/forge.plugin.zsh
@@ -4,7 +4,7 @@
 # Converts '# abc' to '$FORGE_CMD <<< abc' using ZLE widgets
 
 # Configuration: Change this variable to customize the forge command
-FORGE_CMD="cargo run"
+FORGE_CMD="forge"
 
 # Helper function for shared transformation logic
 function _forge_transform_buffer() {
